09054ad1781549b35b102275ceaf166b04d29139
matthewdhull <matthewdhull@gmail.com>
2025-02-08 14:35:12 -0500

    add "-2" option for loaded_iter for initializing empty Gaussian scene

--- scene/__init__.py
+++ scene/__init__.py
@@ -82,7 +82,10 @@ class Scene:
             self.test_cameras[resolution_scale] = cameraList_from_camInfos(scene_info.test_cameras, resolution_scale, args)
 
         if self.loaded_iter:
-            self.gaussians.load_ply(os.path.join(self.model_path,
+            if self.loaded_iter == -2:
+                print("Scene initialized without .ply file")
+            else:
+                self.gaussians.load_ply(os.path.join(self.model_path,
                                                            "point_cloud",
                                                            "iteration_" + str(self.loaded_iter),
                                                            "point_cloud.ply"))
