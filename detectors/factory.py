bf8172a16476ff049830a4c678c938944d2d644d
matthewdhull <matthewdhull@gmail.com>
2025-04-16 22:07:50 -0400

    add yolov5

--- detectors/factory.py
+++ detectors/factory.py
@@ -1,6 +1,7 @@
 from detectors.detectron2_detector import Detectron2Detector
 from detectors.yolov8_detector import Yolov8Detector
 from detectors.yolov3_detector import Yolov3Detector
+from detectors.yolov5_detector import Yolov5Detector
 # from detectors.rtdetr_detector import RTDETRDetector
 # etc.
 
@@ -9,7 +10,9 @@ def load_detector(cfg):
     if backend == "detectron2":
         return Detectron2Detector(cfg)
     elif backend == "yolov3":
-        return Yolov3Detector(cfg)        
+        return Yolov3Detector(cfg)
+    elif backend == "yolov5":
+        return Yolov5Detector(cfg)        
     elif backend == "yolov8":
         return Yolov8Detector(cfg)
     # elif backend == "rtdetr":
